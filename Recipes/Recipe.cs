8d06b9f506f92692dc08b73cd8cf81cb85d51116
Turk <turk.qualls@accenture.com>
2020-01-21 09:45:35 -0600

    Pass - recipe has an empty collection of ingredients

--- Recipes/Recipe.cs
+++ Recipes/Recipe.cs
@@ -1,12 +1,16 @@
+using System.Collections.Generic;
+
 namespace Recipes
 {
     public class Recipe
     {
         public string Name { get; }
-        
+        public List<Ingredient> Ingredients { get; }
+
         public Recipe(string name)
         {
             Name = name;
+            Ingredients = new List<Ingredient>();
         }
     }
 }
