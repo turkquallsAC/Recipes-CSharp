473b9a163e5f7587052e52b761393666d9442df4
Turk <turk.qualls@accenture.com>
2020-01-21 08:36:53 -0600

    Fail - create ingredient with name

--- Recipes.Tests/IngredientTest.cs
+++ Recipes.Tests/IngredientTest.cs
@@ -4,6 +4,13 @@ namespace Recipes.Tests
 {
     public class IngredientTest
     {
-        
+        [Fact]
+        public void IngredientShouldHaveAName()
+        {
+            var ingredientName = "name";
+            var ingredient = new Ingredient(ingredientName);
+            
+            Assert.Equal(ingredientName, ingredient.Name);
+        }
     }
 }
