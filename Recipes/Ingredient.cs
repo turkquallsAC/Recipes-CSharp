d2ed209587be508a60e6c0360b9462a60cb0785b
Turk <turk.qualls@accenture.com>
2020-01-21 08:38:33 -0600

    Pass - constructing ingredient with name

--- Recipes/Ingredient.cs
+++ Recipes/Ingredient.cs
@@ -4,6 +4,11 @@ namespace Recipes
 {
     public class Ingredient
     {
+        public string Name { get; }
         
+        public Ingredient(string name)
+        {
+            Name = name;
+        }
     }
 }
